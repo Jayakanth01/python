7e16fdb3a974d6de7b25a2e6d729e3d517d8fa20
Jayakanth01 <42800530+Jayakanth01@users.noreply.github.com>
2018-09-25 09:30:52 +0530

    Create largestnum.py

---
+++ largestnum.py
@@ -0,0 +1,11 @@
+a = 25
+b= 124
+c = 45
+if (a >= b) and (a >= c):
+   largest = a
+elif (b >= a) and (b >= c):
+   largest = b
+else:
+   largest = c
+ 
+print("The largest num among ",a,",",b,"and",c,"is",largest)
