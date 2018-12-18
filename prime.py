d6deee3acede9f7a49a4c0f503e2dc6fda21c56e
Jayakanth01 <42800530+Jayakanth01@users.noreply.github.com>
2018-12-18 14:42:04 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,14 @@
+num = 227
+
+if num > 1:
+ 
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print("yes")
+       
+else:
+   print("no")
